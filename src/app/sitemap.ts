52f871fedb17943538fc0ded6d8a2b69024ab635
3boudi <vinnyhalitimb47@gmail.com>
2026-05-07 19:21:55 +0100

    SEO

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { MetadataRoute } from 'next';
+import { routing } from '@/i18n/routing';
+
+const host = "https://salonalgeria.netlify.app";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const sitemapEntries: MetadataRoute.Sitemap = [];
+
+  const routes = ['', '/contact'];
+
+  routes.forEach((route) => {
+    routing.locales.forEach((locale) => {
+      sitemapEntries.push({
+        url: `${host}/${locale}${route}`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: route === '' ? 1 : 0.8,
+      });
+    });
+  });
+
+  return sitemapEntries;
+}
